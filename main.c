5d0dff8bbb613b6396ad7c62735e2c1a0985887a
indrajeetb <ibwintheworld@gmail.com>
2017-05-14 11:32:07 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char answer;
+    printf("\n\t\t\tWannaSmile v1.0 - Protect yourself from WannaCry Ransomware\n\n ");
+    printf("\n\t This will do the following");
+    printf("\n\t 1. Disable SMB ( which is enabled by default)");
+    printf("\n\t 2. Add Google's ip to kill-switch (which stops the ransomware from infecting)");
+    printf("\n\n \tTHIS IS A TEMPORARY SOLUTION");
+    printf("\n \tPLEASE UPDATE YOUR WINDOWS  ASAP TO PATCH (MS17-010)");
+    printf("\n\n\tWould you like to make the changes ? Press enter to confirm: ", answer);
+    scanf("%c", &answer);
+    {
+
+
+   system("powershell.exe Set-ItemProperty -Path \"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters\" SMB1 -Type DWORD -Value 0 -Force");
+
+char link[1000]="\n216.58.197.132 www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
+char link1[1000]="\n216.58.197.132 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";;
+   FILE *fptr;
+
+   fptr = fopen("C:\\Windows\\System32\\drivers\\etc\\hosts", "a");
+
+   fprintf(fptr,"%s", link);
+   fprintf(fptr,"%s", link1);
+   fclose(fptr);
+   exit(0);
+    }
+   return 0;
+}
