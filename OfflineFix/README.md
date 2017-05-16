a9cfaf0c3e5df5ac14d40853929debce99f2b467
geekodour <hrishikeshbman@gmail.com>
2017-05-16 19:22:42 +0530

    added new fixes and kill switches to the offline fix

--- OfflineFix/README.md
+++ OfflineFix/README.md
@@ -4,13 +4,19 @@ Runs a local server and localhost to the wannaCry kill-switch by appending hosts
 ## Instructions
 - Install the `wannaSmile` service by running the `setup.exe` from [this release](https://github.com/indrajeetb/WannaSmile/releases/tag/0.1). (Download the `wannasmile.zip` file)
 - After Installing you need to start the service once and then it will do the rest automatically
-- To do that 
+- The easiest way to do that is to simply **restart your computer**. *(Recommended)* 
+
+- The other *not so easy* way is to do the following
 	- Open start menu
 	- Search `services`
 	- Open the `Services` desktop app (a gear icon)
 	- Inside `Services` search for `WannaSmile` (The list is alphabatical)
 	- `right click` on WannaSmile and click `start`
 - The service will be running and the wanna cry IPs will be blocked along with the SMBs
+
+## Uninstall Instructions
+When you think there is no more danger from wannaCry, you can simply uninstall WannaSmile
+- Simply go to `control panel` and click `uninstall a program` and uninstall it.
 
 ## Issues
 - Other than security concerns there is one [issue](https://github.com/indrajeetb/WannaSmile/issues/1) with the host file appending [HELP WANTED]
--- OfflineFix/src/wannaSmile/Service1.cs
+++ OfflineFix/src/wannaSmile/Service1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
 using System.Net;
@@ -18,6 +12,7 @@ namespace wannaSmile
     {
         Thread th  = new Thread(DoThis);
         static bool isRunning = false;
+        static bool IPBlocked = false;
         static HttpListener server = new HttpListener();
 
         public Service1()
@@ -27,23 +22,48 @@ namespace wannaSmile
 
         protected override void OnStart(string[] args)
         {
-            // do powershell thing
+            // powershell command to disable SMBs
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             process.StartInfo.FileName = "powershell.exe";
             process.StartInfo.Arguments = "Set-ItemProperty -Path \"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters\" SMB1 -Type DWORD -Value 0 -Force; Set-ItemProperty -Path \"HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters\" SMB2 -Type DWORD -Value 0 -Force";
             process.Start();
             // end powershell
+
+            // check if etc\hosts already blocks wannacry domains
+            string line;
+            string domain = "127.0.0.1 www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Windows\\System32\\drivers\\etc\\hosts");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == domain)
+                {
+                    IPBlocked = true;
+                    break;
+                }
+            }
+
+            file.Close();
+      
             // update hosts files
-            File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com" + Environment.NewLine);
-            File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com" + Environment.NewLine);
+            if (!IPBlocked)
+            {
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 www.ifferfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 ifferfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 www.iuqssfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 iuqssfsodp9ifjaposdfjhgosurijfaewrwergwea.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 www.ayylmaotjhsstasdfasdfasdfasdfasdfasdfasdf.com");
+                File.AppendAllText(@"C:\\Windows\\System32\\drivers\\etc\\hosts", "\n127.0.0.1 ayylmaotjhsstasdfasdfasdfasdfasdfasdfasdf.com");
+            }
+
             //end updating
+
             isRunning = true;
             server.Prefixes.Add("http://127.0.0.1/");
             server.Prefixes.Add("http://localhost/");
             server.Start(); 
-            //HttpListenerContext context = server.GetContext();
-            //HttpListenerResponse response = context.Response;
             th.Start();
         }
 
@@ -57,8 +77,8 @@ namespace wannaSmile
         static void handleRequest()
         {
             HttpListenerContext context = server.GetContext();
-            byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>wannasmile</title></head>" +
-            "<body>safe from wannacry :)</body></html>"); 
+            byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>WannaSmile</title></head>" +
+            "<body>Safe from WannaCry :) <a href=\"https://github.com/indrajeetb/WannaSmile\">- wannasmile</a></body></html>"); 
             context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length); 
             context.Response.KeepAlive = false; 
             context.Response.Close();
@@ -70,6 +90,5 @@ namespace wannaSmile
             th = null;
         }
     }
-    // end of windows service 
-  
+    // end of windows service
 }
